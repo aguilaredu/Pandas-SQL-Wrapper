6769afb9a8166f0c118692c11539230ab786957a
Eduardo Aguilar <git@eduaguilar.com>
2022-05-29 13:01:10 -0600

    Fixed fill dataframe nulls function in dfutils

--- src/dfutils.py
+++ src/dfutils.py
@@ -1,8 +1,8 @@
 from pandas import DataFrame
-from numpy import nan
+import numpy as np
 
-def fill_dataframe_nulls(df: DataFrame, *args) -> DataFrame:
-    """Fills DataFrame with null-like values with None. Default list to replace is [np.nan, 'NULL', 'nan'].
+def fill_dataframe_nulls(df: DataFrame, *args):
+    """Fills the dataframe with NULL like values with None. Default list to replace is [np.nan, 'NULL', 'nan'].
     It is possible to add more values to the list using the *args parameter passing the values after the required arguments
 
     Args:
@@ -12,7 +12,7 @@ def fill_dataframe_nulls(df: DataFrame, *args) -> DataFrame:
         [Dataframe]: The dataframe with the replaced values. 
     """
 
-    null_list = [['null'], ['NULL'], ['nan'], ['NAN'], ['None'], [nan]]
+    null_list = [['NULL'], ['nan'], [np.nan]]
     
     # Adds optional items to the null key
     for i in range(len(args)):
@@ -20,4 +20,11 @@ def fill_dataframe_nulls(df: DataFrame, *args) -> DataFrame:
 
     # Iterate through the null list and replace with np.nan
     for null_value in null_list:
-        df.replace([null_value], [nan], inplace=True)
+        df.replace([null_value], [np.nan], inplace=True)
+
+    # Fill NAs with np.nan because pandas could've inferred datatypes for certain columns and 
+    # replaced the np.nans that were filled in the last step with np.NaT, etc. Then replace the 
+    # np.nan with None 
+    return_df = df.fillna(np.nan).replace([np.nan], [None])
+
+    return return_df
